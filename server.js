51eeaaae0d8f48e757adcffc253a7a4cec6b4b86
smitayevale <66351654+smitayevale@users.noreply.github.com>
2020-06-04 08:59:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ module.exports = {
                 fs.readFile("./details.html","utf8",function(err,data){
                         if(err) throw err;
                         q = urlinfo.query
-                        eid = q.e_id;
+                        eid = q.id;
 							
                         pool.query("SELECT e.employee_id, e.name, e.salary, d.dept_name, d.location FROM employee e INNER JOIN department d on d.dept_no = e.dept_no WHERE e.employee_id = '"+eid+"';", function (err, result) {
                         if (err) throw err;
                         res.writeHead(200,{"content-type":"text/html"},result.rows);
-                        res.write("<label for='eid'>Employee Id:</label><br><br><input type='text' id='eid' name='e_id' value='"+result.rows[0].employee_id+"'><br><br><label for='eame'>Employee Name:</label><br><br><input type='text' id='ename' name='e_name' value='"+result.rows[0].name+"'><br><br><label for='pid'>Salary:</label><br><br><input type='text' id='pid' name='p_id' value='"+result.rows[0].salary+"'><br><br><label for='oid'>Department:</label><br><br><input type='text' id='oid' name='o_id' value='"+result.rows[0].dept_name+"'><br><br><label for='amt'>Location:</label><br><br><input type='text' id='amt' name='amt' value='"+result.rows[0].location+"'><br><br>");
+                        res.write("<label for='employee_id'>Employee Id:</label><br><br><input type='text' id='employee_id' name='employee_id' value='"+result.rows[0].employee_id+"'><br><br><label for='name'>Employee Name:</label><br><br><input type='text' id='name' name='name' value='"+result.rows[0].name+"'><br><br><label for='salary'>Salary:</label><br><br><input type='text' id='salary' name='salary' value='"+result.rows[0].salary+"'><br><br><label for='dept_name'>Department:</label><br><br><input type='text' id='dept_name' name='dept_name' value='"+result.rows[0].dept_name+"'><br><br><label for='location'>Location:</label><br><br><input type='text' id='location' name='location' value='"+result.rows[0].location+"'><br><br>");
                         console.log(result.rows[0].name);
                         res.end();
                         });
